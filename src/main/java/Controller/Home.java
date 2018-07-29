e6464640c332f45767219a3d3ee65e21ebbc77d7
Vignesh Rajkumar <vigneshrajkumar@hotmail.com>
2018-07-29 17:37:45 +0530

    Fixed UI startup issue

--- src/main/java/Controller/Home.java
+++ src/main/java/Controller/Home.java
@@ -1,34 +1,39 @@
 package Controller;
 
-import Model.ApplicantInput;
-import Model.ApplicantRecord;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.List;
 
-public class Home {
+public class Home extends Application {
 
     public static void main(String[] args) {
-        System.out.println("yo");
-
-
-
-        List<ApplicantInput> applicantInputs = CSVParser.getApplicantInputs("input.csv");
-
-        for (ApplicantInput applicantInput : applicantInputs){
-            ApplicantRecord record = ReportBuilder.generateReport(applicantInput);
-
-//            System.out.println(record);
-
-            PDFGenerator.generatePDF(record);
-        }
-
-
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+//        List<ApplicantInput> applicantInputs = CSVParser.getApplicantInputs("input.csv");
+//
+//        for (ApplicantInput applicantInput : applicantInputs){
+//            ApplicantRecord record = ReportBuilder.generateReport(applicantInput);
+//
+////            System.out.println(record);
+//
+//            PDFGenerator.generatePDF(record);
+//        }
+
+        System.out.println("Ping");
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
+
+        Scene newScene = new Scene(root, 472,128);
+        primaryStage.setScene(newScene);
+        primaryStage.setTitle("My Life My Future");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 }
